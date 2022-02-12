e31cf16b675ed28938b9fe57a72b8d7a3ca8568b
FekyDEV <71899549+FekyDEV@users.noreply.github.com>
2022-02-12 22:14:32 +0100

    Update fxmanifest.lua

--- feky_anticheat/fxmanifest.lua
+++ feky_anticheat/fxmanifest.lua
@@ -1,11 +1,8 @@
--- This resource is part of the default Cfx.re asset pack (cfx-server-data)
--- Altering or recreating for local use only is strongly discouraged.
+version '1.1.0'
+author 'FekyDEV'
+description 'Anti- Infinity Ammo'
 
-version '1.0.0'
-author 'cs'
-description 'cs.'
-
-server_script 'server.lua'
+--server_script 'server.lua'
 client_script 'client.lua'
 
 game 'common'
